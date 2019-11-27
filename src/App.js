c5bcedae2c34aac0044ee621a9dab93b0f6c9017
Ksusha Zhukova <zhukovaksusha@gmail.com>
2019-11-27 21:29:40 +0300

    add page outline

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Search } from "./pages/search";
+import { ThemeProvider } from "emotion-theming";
+import styled from "@emotion/styled";
+import { Global } from "@emotion/core";
+const theme = {
+  colors: {
+    primary: "black"
+  }
+};
 
-function App() {
+const Header = styled.header`
+  height: 10%;
+  width: 100%;
+  background-color: hotpink;
+`;
+
+const Main = styled.div`
+  width: 100%;
+  background-color: red;
+  height: 90%;
+`;
+
+export default () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <Global
+        styles={{
+          html: {
+            height: "100%"
+          },
+          body: {
+            height: "100%"
+          },
+          "#root": {
+            height: "100%"
+          }
+        }}
+      />
+      <ThemeProvider theme={theme}>
+        <Header></Header>
+        <Main>
+          <Search />
+        </Main>
+      </ThemeProvider>
+    </body>
   );
-}
-
-export default App;
+};
---
+++ src/pages/search.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import styled from "@emotion/styled";
+
+const Block = styled.div`
+  height: 10%;
+  width: 800px;
+  background-color: yellow;
+  margin: auto;
+`;
+export const Search = () => {
+  return <Block>Hello world</Block>;
+};
